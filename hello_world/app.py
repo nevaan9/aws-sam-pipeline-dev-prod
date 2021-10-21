8330f2e264bf874cba43a1b380f5058cc38aa7da
Nevaan Perera <nevaan9@gmail.com>
2021-10-21 17:16:34 -0400

    prod check

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "CODEPIPELINE SCIENCE, BITCH!!!!",
+            "message": "CODEPIPELINE SCIENCE, BITCH!!!!, prod!",
             # "location": ip.text.replace("\n", "")
         }),
     }
