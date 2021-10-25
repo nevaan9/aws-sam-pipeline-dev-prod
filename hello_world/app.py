96078b5592f3576c6896e8d21ce7655b62e24f81
Nevaan Perera <nevaan9@gmail.com>
2021-10-25 15:31:19 -0400

    added master branch 2

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "HELLO FROM MASTER!!",
+            "message": "HELLO FROM MASTER ROUND 2!!!!",
             # "location": ip.text.replace("\n", "")
         }),
     }
